cb6c20d8fb5c5e2fda7fdae5cb6b00f5e15988f5
Muhammad Hutomo Padmanaba <mhutomop@gmail.com>
2025-01-20 08:38:25 +0700

    feat(query): filter using range

--- src/app.cpp
+++ src/app.cpp
@@ -1,32 +1,13 @@
 #include <array>
 #include <chrono>
 #include <iostream>
-#include <aerospike/aerospike.h>
-#include <aerospike/aerospike_key.h>
-#include <aerospike/as_exp.h>
-#include <aerospike/as_status.h>
-#include <aerospike/as_record.h>
-#include <aerospike/as_query.h>
-#include <aerospike/aerospike_info.h>
-#include <aerospike/aerospike_query.h>
-#include "exp_function/exp_function.h"
+#include "operations/operations.h"
 
 int64_t get_time_in_us() {
     auto now = std::chrono::high_resolution_clock::now();
     auto duration = std::chrono::duration_cast<std::chrono::microseconds>(now.time_since_epoch());
 
     return duration.count();
-}
-
-// Callback function to process query results
-bool query_callback(const as_val *val, void *udata) {
-    if (!val) return false; // No more results
-
-    // Cast the value to a record
-    if (const as_record *rec = as_record_fromval(val)) {
-        std::cout << "Record read successfully. Title value: " << as_record_get_str(rec, "title") << std::endl;
-    }
-    return true; // Continue processing
 }
 
 int main() {
@@ -52,93 +33,38 @@ int main() {
     const char *ns = "test";
     const char *set = "books";
     const uint16_t number_of_records = 10000;
-
-    // Create index
-    if (aerospike_index_string_create(&as, &err, nullptr, "test", "books", "author", "idx_author") != AEROSPIKE_OK) {
-        printf("aerospike_index_integer_create() returned %d - %s", err.code, err.message);
-        return false;
-    }
-
-    if (aerospike_index_string_create(&as, &err, nullptr, "test", "books", "title", "idx_title") != AEROSPIKE_OK) {
-        printf("aerospike_index_integer_create() returned %d - %s", err.code, err.message);
-        return false;
-    }
-
-    // Create records
-    std::cout << "=================================================================================" << std::endl;
-    std::cout << "CREATE 10,0000 RECORDS" << std::endl;
-    auto start = get_time_in_us();
     std::array<as_key, number_of_records> record_keys;
     std::array<as_record, number_of_records> records;
-    for (uint16_t i = 0; i < number_of_records; i++) {
-        std::string key = "book" + std::to_string(i);
-        std::string author = "author" + std::to_string(i);
-        std::string title = "title" + std::to_string(i);
 
-        as_key_init(&record_keys[i], ns, set, key.c_str());
+    create_indexes(&as, ns, set);
 
-        as_record_inita(&records[i], 3);
-        
-        as_record_set_str(&records[i], "author", author.c_str());
-        as_record_set_str(&records[i], "title", title.c_str());
-        as_record_set_int64(&records[i], "published_year", 1997);
-
-        // Set write policy with send_key = true
-        as_policy_write policy;
-        as_policy_write_init(&policy);
-        policy.key = AS_POLICY_KEY_SEND;  // Ensure the primary key is sent with the write request
-
-
-        if (aerospike_key_put(&as, &err, &policy, &record_keys[i], &records[i]) != AEROSPIKE_OK) {
-            std::cerr << "Error writing record: " << err.message << std::endl;
-        }
-    }
+    std::cout << "=================================================================================" << std::endl;
+    auto start = get_time_in_us();
+    create_records(&as, ns, set, number_of_records, record_keys.data(), records.data());
     std::cout << "Time: " + std::to_string(get_time_in_us() - start) << std::endl;
 
     // Read the records by primary key
     std::cout << "=================================================================================" << std::endl;
-    std::cout << "SEARCH BY PRIMARY KEY" << std::endl;
     start = get_time_in_us();
-    as_key record_key;
-    as_key_init(&record_key, ns, set, "book100");
-    if (as_record *read_record = nullptr; aerospike_key_get(&as, &err, nullptr, &record_key, &read_record) == AEROSPIKE_OK) {
-        std::cout << "Record read successfully. Title value: "
-                  << as_record_get_str(read_record, "title") << std::endl;
-    } else {
-        std::cerr << "Error reading record: " << err.message << std::endl;
-    }
+    search_by_primary_key(&as, ns, set, "book100");
     std::cout << "Time: " + std::to_string(get_time_in_us() - start) << std::endl;
 
-    // Read the records by bin value
+    // Read the records by multiple bin value
     std::cout << "=================================================================================" << std::endl;
-    std::cout << "SEARCH BY BIN VALUE" << std::endl;
     start = get_time_in_us();
-    // Prepare the query
-    as_query query;
-    as_exp* exp = get_exp_filter_author_and_title("author100", "title100");
-    as_policy_query policy;
-    policy.base.filter_exp = exp;
-    as_query_init(&query, ns, set); // Namespace: "test", Set: "books"
-
-    // Execute the query
-    if (aerospike_query_foreach(&as, &err, &policy, &query, query_callback, nullptr) != AEROSPIKE_OK) {
-        std::cerr << "Error reading record: " << err.message << std::endl;
-    }
+    search_by_multiple_bin_value(&as, ns, set, "author100", "title100");
     std::cout << "Time: " + std::to_string(get_time_in_us() - start) << std::endl;
-    as_query_destroy(&query);
+
+    // Read the records by bin value with range
+    std::cout << "=================================================================================" << std::endl;
+    start = get_time_in_us();
+    search_by_bin_value_with_range(&as, ns, set, 2000, 2005);
+    std::cout << "Time: " + std::to_string(get_time_in_us() - start) << std::endl;
 
     // Delete the records
     std::cout << "=================================================================================" << std::endl;
-    std::cout << "DELETE RECORDS" << std::endl;
     start = get_time_in_us();
-    for (uint16_t i = 0; i < number_of_records; i++) {
-        if (aerospike_key_remove(&as, &err, nullptr, &record_keys[i]) != AEROSPIKE_OK) {
-            std::cerr << "Error deleting record: " << err.message << std::endl;
-        }
-
-        as_key_destroy(&record_keys[i]);
-        as_record_destroy(&records[i]);
-    }
+    delete_records(&as, number_of_records, record_keys.data(), records.data());
     std::cout << "Time: " + std::to_string(get_time_in_us() - start) << std::endl;
 
     // Cleanup
---
+++ src/operations/operations.h
@@ -0,0 +1,64 @@
+#include <aerospike/aerospike.h>
+#include <aerospike/aerospike_key.h>
+#include <aerospike/as_arraylist.h>
+#include <aerospike/as_exp.h>
+#include <aerospike/as_hashmap.h>
+#include <aerospike/as_status.h>
+#include <aerospike/as_record.h>
+#include <aerospike/as_query.h>
+#include <aerospike/aerospike_info.h>
+#include <aerospike/aerospike_query.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+    
+void create_indexes
+(
+    aerospike* as,
+    const char* ns,
+    const char* set
+);
+void create_records
+(
+    aerospike* as,
+    const char* ns,
+    const char* set,
+    uint16_t number_of_records,
+    as_key* record_keys,
+    as_record* records
+);
+void search_by_primary_key
+(
+    aerospike* as,
+    const char* ns,
+    const char* set,
+    const char* key
+);
+void search_by_multiple_bin_value
+(
+    aerospike* as,
+    const char* ns,
+    const char* set,
+    const char* author,
+    const char* title
+);
+void search_by_bin_value_with_range
+(
+    aerospike* as,
+    const char* ns,
+    const char* set,
+    const uint16_t published_year_start,
+    const uint16_t published_year_end
+);
+void delete_records
+(
+    aerospike* as,
+    uint16_t number_of_records,
+    as_key* record_keys,
+    const as_record* records
+);
+
+#ifdef __cplusplus
+}
+#endif
